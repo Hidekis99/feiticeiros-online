6e57f5b332ef58e7400c2a7a4644d2a4eaee60d1
Gustavo Soares <gustavosoares59752@gmail.com>
2026-02-13 12:09:00 -0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,27 @@
+import { auth } from "./firebase.js";
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword } 
+from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
+document.getElementById("loginBtn").onclick = async () => {
+  const email = email.value;
+  const senha = senha.value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, senha);
+    window.location.href = "dashboard.html";
+  } catch (err) {
+    alert(err.message);
+  }
+};
+
+document.getElementById("registerBtn").onclick = async () => {
+  const email = email.value;
+  const senha = senha.value;
+
+  try {
+    await createUserWithEmailAndPassword(auth, email, senha);
+    window.location.href = "dashboard.html";
+  } catch (err) {
+    alert(err.message);
+  }
+};
