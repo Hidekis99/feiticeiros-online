aa8b5b8ea67ca94dd161c6811f3c0a4f9c182960
Gustavo Soares <gustavosoares59752@gmail.com>
2026-02-13 12:14:59 -0300

    Update ficha.js

--- js/ficha.js
+++ js/ficha.js
@@ -7,3 +7,31 @@ document.querySelectorAll(".tab").forEach(tab => {
     document.getElementById(tab.dataset.tab).classList.add("active");
   };
 });
+import { db } from "./firebase.js";
+import { collection, getDocs, doc, setDoc } 
+from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+let especializacoesData = {};
+let characterId = new URLSearchParams(window.location.search).get("id");
+
+async function carregarEspecializacoes() {
+  const snapshot = await getDocs(collection(db, "specializations"));
+
+  const container = document.getElementById("listaEspecializacoes");
+
+  snapshot.forEach(docSnap => {
+    const data = docSnap.data();
+    especializacoesData[docSnap.id] = data;
+
+    const div = document.createElement("div");
+
+    div.innerHTML = `
+      <label>${data.nome}</label>
+      <input type="number" min="0" value="0" id="esp-${docSnap.id}">
+    `;
+
+    container.appendChild(div);
+  });
+}
+
+carregarEspecializacoes();
