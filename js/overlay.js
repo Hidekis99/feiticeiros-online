6d286223ac613be65396275d65e10ba869180b92
Gustavo Soares <gustavosoares59752@gmail.com>
2026-02-13 12:37:22 -0300

    Update overlay.js

--- js/overlay.js
+++ js/overlay.js
@@ -1,1 +1,20 @@
+import { db } from "./firebase.js";
+import { doc, onSnapshot } 
+from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
+const id = new URLSearchParams(location.search).get("id");
+
+onSnapshot(doc(db, "characters", id), snap => {
+  const data = snap.data();
+  if (!data) return;
+
+  nome.innerText = data.nome || "";
+
+  if (data.hp) {
+    hpBar.style.width = (data.hp.atual / data.hp.base) * 100 + "%";
+  }
+
+  if (data.energia) {
+    energiaBar.style.width = (data.energia.atual / data.energia.base) * 100 + "%";
+  }
+});
