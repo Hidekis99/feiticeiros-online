9689ddf30e39083bb9c77a7609ecc82ef95b7cac
Gustavo Soares <gustavosoares59752@gmail.com>
2026-02-13 12:11:38 -0300

    Update ficha.js

--- js/ficha.js
+++ js/ficha.js
@@ -1,1 +1,9 @@
+document.querySelectorAll(".tab").forEach(tab => {
+  tab.onclick = () => {
+    document.querySelectorAll(".tab").forEach(t => t.classList.remove("active"));
+    document.querySelectorAll(".tab-content").forEach(c => c.classList.remove("active"));
 
+    tab.classList.add("active");
+    document.getElementById(tab.dataset.tab).classList.add("active");
+  };
+});
